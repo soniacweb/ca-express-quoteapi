abf0ad89da96c59ed2174834421c4e0964218f91
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-12-04 19:33:53 +0000

    completed mandatory exercises

--- public/add-quote.js
+++ public/add-quote.js
@@ -1,22 +1,23 @@
-const submitButton = document.getElementById('submit-quote');
-const newQuoteContainer = document.getElementById('new-quote');
+const submitButton = document.getElementById('submit-quote')
+const newQuoteContainer = document.getElementById('new-quote')
 
 submitButton.addEventListener('click', () => {
-  const quote = document.getElementById('quote').value;
-  const person = document.getElementById('person').value;
+  const quote = document.getElementById('quote').value
+  const person = document.getElementById('person').value
 
   fetch(`/api/quotes?quote=${quote}&person=${person}`, {
-    method: 'POST',
+    method: 'POST'
   })
-  .then(response => response.json())
-  .then(({quote}) => {
-    const newQuote = document.createElement('div');
-    newQuote.innerHTML = `
+    .then(response => response.json())
+    .then(() => {
+
+      const newQuote = document.createElement('div')
+      newQuote.innerHTML = `
     <h3>Congrats, your quote was added!</h3>
-    <div class="quote-text">${quote.quote}</div>
-    <div class="attribution">- ${quote.person}</div>
     <p>Go to the <a href="index.html">home page</a> to request and view all quotes.</p>
-    `
-    newQuoteContainer.appendChild(newQuote);
-  });
-});
+    ` 
+      // <div class="quote-text">${quote.quote}</div>
+      // <div class="attribution">- ${quote.person}</div>
+      newQuoteContainer.appendChild(newQuote)
+    })
+})
--- server.js
+++ server.js
@@ -4,7 +4,73 @@ const app = express()
 const { quotes } = require('./data')
 const { getRandomElement } = require('./utils')
 
-const PORT = process.env.PORT || 4001
+const PORT = process.env.PORT || 8000
 
 app.use(express.static('public'))
 
+//listening to port
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+//get random quotes
+app.get('/api/quotes/random', (req, res, next) => {
+  // Here we would send back the moods array in response
+  const randomQuote = getRandomElement(quotes)
+  if (randomQuote) {
+    res.send(
+      { 
+        quote: randomQuote 
+      })
+  } else {
+    res.status(404).send()
+  }
+  
+})
+
+//get all quotes or send error if no quotes
+// app.get('/api/quotes', (req, res, next) => {
+//   const allQuotes = { quotes: quotes }
+//   if (allQuotes) {
+//     res.send(
+//       { 
+//         quote: allQuotes 
+//       })
+//   } else {
+//     res.status(404).send()
+//   }
+// })
+
+//by author
+app.get('/api/quotes', (req, res, next) => {
+  const reqPerson = req.query.person
+  if (!reqPerson) {
+    res.send({ quotes: quotes })
+    next()
+  } else if (reqPerson) {
+    const filteredSearch = quotes.filter(quote => quote.person === reqPerson)
+    console.log(filteredSearch)
+    res.send({ quotes: filteredSearch }) 
+  } 
+})
+
+//post new quote
+app.post('/api/quotes', (req, res) => {
+  const newEntry = req.query.quote && req.query.person.length
+  if (newEntry) {
+    quotes.push(req.query)
+    res.status(201).send({ quotes: req.query })
+  } else {
+    res.status(400).send('You must enter a quote and an author')
+  }
+})
+
+//put request 
+app.put('/api/quotes/:id', (req, res) => {
+
+})
+
+//delete request
+app.delete('/api/quotes/:id', (req, res) => {
+
+})
